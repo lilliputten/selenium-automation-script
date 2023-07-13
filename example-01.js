86ac6fc06bc684dcc094147974be8bcf4290d768
lilliputten <lilliputten@yandex.ru>
2023-07-14 00:01:04 +0700

    First working version.

---
+++ example-01.js
@@ -0,0 +1,13 @@
+const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+  try {
+    await driver.get('https://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.wait(() => {}, 5000);
+  } finally {
+    await driver.quit();
+  }
+})();
